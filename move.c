fd4f90099b26268b2679b78f5331d2eb7eaf1d54
Kuba <kuba@MacBook-Air-Kuba.local>
2023-06-13 12:49:09 +0200

    minor tweaks and improved readability

--- move.c
+++ move.c
@@ -19,8 +19,8 @@ typedef struct {
     int training_time;
 } au;
 
-void move(char fname[], au a[], int* u, int m[][MAP_SIZE_X]) {
-    
+void move(char fname[], au a[], int* u, int m[][MAP_SIZE_X])
+{   
     int allowed_movement; // unit's movement points
     int id_counter; // validates the id indicated by the user
     int coord_counter; // monitors if two coordinates were given
@@ -32,10 +32,13 @@ void move(char fname[], au a[], int* u, int m[][MAP_SIZE_X]) {
     char* unit; // unit type retrieved from active_units    
 
     id_counter = 0;
-    if (*u <= 2) {
+    if (*u <= 2)
+    {
         printf("\nThere are no units capable of moving.\n");
         printf("Type MENU to go back to main menu.\n");
-    } else {
+    }
+    else
+    {
         /* Requesting user input: indicating the unit to be moved */
         while (id_counter < 1) {
             printf("Which unit do you intend to move? (ID): ");
@@ -50,33 +53,40 @@ void move(char fname[], au a[], int* u, int m[][MAP_SIZE_X]) {
 
     coord_counter = 0; 
     if (id_counter > 0) {
-        if (strcmp(a[id].affiliation, "E") == 0) {
-        
+        if (strcmp(a[id].affiliation, "E") == 0)
+        {
             printf("\nCannot select enemy units.\n");
             printf("Type MENU to go back to main menu.\n");
-        
-        } else if ((strcmp(a[id].affiliation, "P") == 0) && (a[id].training_time > 0)) {
-        
+        }
+        else if ((strcmp(a[id].affiliation, "P") == 0) && (a[id].training_time > 0))
+        {
             printf("\nCannot move units undergoing training.\n");
             printf("Type MENU to go back to main menu.\n");
-        
-        } else {
-        
-            while (coord_counter < 2) {    
+        }
+        else
+        {
+            while (coord_counter < 2)
+            {    
             /* requesting target coordinates and validating input*/
             printf("Please specify the target X coordinate: ");
-                if (scanf(" %d", &x) == 1 && (x >= 0 && x <= MAP_SIZE_X - 1)) {
+                if (scanf(" %d", &x) == 1 && (x >= 0 && x <= MAP_SIZE_X - 1))
+                {
                     coord_counter++;
                     //printf("\nCoord counter: %d\n", coord_counter);    
-                } else {
+                }
+                else
+                {
                     printf("\nX coordinate needs to range from 0 to %d.\n", MAP_SIZE_X - 1);
                     continue;
                 }
             printf("Please specify the target Y coordinate: ");
-                if (scanf(" %d", &y) == 1 && (y >= 0 && y <= MAP_SIZE_Y - 1)) {
+                if (scanf(" %d", &y) == 1 && (y >= 0 && y <= MAP_SIZE_Y - 1))
+                {
                     coord_counter++;
                     //printf("\nCoord counter: %d\n", coord_counter);
-                } else {
+                }
+                else
+                {
                     printf("\nY coordinate needs to range from 0 to %d.\n", MAP_SIZE_Y - 1);
                     continue;
                 }
@@ -84,66 +94,73 @@ void move(char fname[], au a[], int* u, int m[][MAP_SIZE_X]) {
         }    
     }
 
-        /* prevent the unit from going on obstacles */
-    if (coord_counter >= 2) {
-        if (m[y][x] == 9) {
-
+    /* prevent the unit from going on obstacles */
+    if (coord_counter >= 2)
+    {
+        if (m[y][x] == 9)
+        {
             printf("\nThis field is a natural obstacle.\n");
             printf("Type MENU to go back to main menu.\n");
             map_field_occupied = 1;
         
-        } else if (m[y][x] == 2) {
-        
-        /* prevent the unit from going on an enemy-occupied field  */
+        }
+        else if (m[y][x] == 2)
+        {
+            /* prevent the unit from going on an enemy-occupied field  */
             printf("\nThis field is an enemy base.\n");
             map_field_occupied = 1;
-        
-        } else if (m[y][x] == 0) {
-    
+        } else if (m[y][x] == 0)
+        {
             for (int i = 0; i < *u; i++)
-		        if ((strcmp(a[i].affiliation, "E") == 0) && (a[i].x_coord == x) && (a[i].y_coord == y)) {
+		        if ((strcmp(a[i].affiliation, "E") == 0) && (a[i].x_coord == x) && (a[i].y_coord == y))
+                {
     	            map_field_occupied = 1;
                 }
-    
-            if (map_field_occupied == 1) {
+            if (map_field_occupied == 1)
+            {
                 printf("\nThis is enemy-held territory.\n\n");
                 printf("Type MENU to go back to main menu.\n");
             }
         }
     }
 
-        /* validating the distance a unit may travel*/
-        if (map_field_occupied < 1) {
+    /* validating the distance a unit may travel*/
+    if ((map_field_occupied < 1) && (coord_counter != 0))
+    {
+        distance = abs(a[id].x_coord - x) + abs(a[id].y_coord - y);
 
-            distance = abs(a[id].x_coord - x) + abs(a[id].y_coord - y);
-
-            if (strcmp(a[id].unit_type, "K") == 0)
-                allowed_movement = 5;
-            else
-                allowed_movement = 2;
+        if (strcmp(a[id].unit_type, "K") == 0)
+            allowed_movement = 5;
+        else
+            allowed_movement = 2;
             
-            if ((distance > a[id].remaining_movement) || (distance > allowed_movement)) {
-                printf("Distance exceeds the unit's range.\n");
-                printf("Type MENU to go back to main menu.\n");
-            } else if ((distance <= allowed_movement) && (distance <= a[id].remaining_movement)) {
-                a[id].remaining_movement = allowed_movement - distance;
-                printf("Ordering unit %d to move to X: %d, Y: %d.\n", id, x, y);
-
-                /* order successful, write to file */
-                FILE *fptr;
-
-                fptr = fopen(fname, "a");
-                if (!fptr) {
-                    printf("Cannot open rozkazy.txt");
-                }
-
-                if (fprintf(fptr, "%d M %d %d\n", id, x, y) < 0) {
-                    fprintf(stderr, "\nUnable to write order to file.\n");
-                }
-
-                fclose(fptr);
-
-                printf("Type MENU to go back to main menu.\n");
-            } 
+        if ((distance > a[id].remaining_movement) || (distance > allowed_movement))
+        {
+            printf("Distance exceeds the unit's range.\n");
+            printf("Type MENU to go back to main menu.\n");
         }
+        else if ((distance <= allowed_movement) && (distance <= a[id].remaining_movement))
+        {
+            a[id].remaining_movement = allowed_movement - distance;
+            printf("Ordering unit %d to move to X: %d, Y: %d.\n", id, x, y);
+
+            /* order successful, write to file */
+            FILE *fptr;
+
+            fptr = fopen(fname, "a");
+            if (!fptr)
+            {
+                printf("Cannot open rozkazy.txt");
+            }
+
+            if (fprintf(fptr, "%d M %d %d\n", id, x, y) < 0)
+            {
+                fprintf(stderr, "\nUnable to write order to file.\n");
+            }
+
+            fclose(fptr);
+
+            printf("Type MENU to go back to main menu.\n");
+        } 
     }
+}
